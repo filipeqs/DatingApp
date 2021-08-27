68b1d3839b13a7f4978eb3d949c82d0aea70efb0
Filipe Silva <filipeqs@outlook.com>
2021-08-27 18:48:38 -0400

    list of likes and liked by

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../models/member';
+import { Pagination } from '../models/pagination';
+import { MembersService } from '../_services/members.service';
 
 @Component({
-  selector: 'app-lists',
-  templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+    selector: 'app-lists',
+    templateUrl: './lists.component.html',
+    styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent implements OnInit {
+    members: Partial<Member[]>;
+    predicate = 'liked';
+    pageNumber = 1;
+    pageSize = 5;
+    pagination: Pagination;
 
-  constructor() { }
+    constructor(private membersService: MembersService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.loadLikes();
+    }
 
+    loadLikes() {
+        this.membersService
+            .getLikes(this.predicate, this.pageNumber, this.pageSize)
+            .subscribe((response) => {
+                this.members = response.result;
+                this.pagination = response.pagination;
+            });
+    }
+
+    pageChanged(event: any) {
+        this.pageNumber = event.page;
+        this.loadLikes();
+    }
 }
