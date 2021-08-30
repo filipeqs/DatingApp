3b2ee8c028d9405c72c10756518be2a03c8fc368
Filipe Silva <filipeqs@outlook.com>
2021-08-29 20:20:09 -0400

    applied fix

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -14,14 +14,21 @@ export class MemberMessagesComponent implements OnInit {
     @Input() messages: Message[];
     @Input() username: string;
     messageContent: string;
+    loading = false;
 
     constructor(public messageService: MessageService) {}
 
     ngOnInit(): void {}
 
     sendMessage() {
-        this.messageService.sendMessage(this.username, this.messageContent).then(() => {
-            this.messageForm.reset();
-        });
+        this.loading = true;
+        this.messageService
+            .sendMessage(this.username, this.messageContent)
+            .then(() => {
+                this.messageForm.reset();
+            })
+            .finally(() => {
+                this.loading = false;
+            });
     }
 }
